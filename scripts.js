b0a4cba29886ae0c1e723ead0f15c74bb0353d6c
ManiacalMaynard <maynard@maniacalindustries.com>
2022-05-16 16:58:50 -0700

    completes basics

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Show and hide the poster modal
+const showModal = () => {
+  document.getElementById('posterModal').style.visibility = 'visible' ;
+}
+const hideModal = () => {
+  document.getElementById('posterModal').style.visibility = 'hidden' ;
+};
